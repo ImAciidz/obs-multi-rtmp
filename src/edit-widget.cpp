6c03a6dba28be87b2c0a5cd6a55089a50d22716f
Hayden K <imaciidz@gmail.com>
2023-03-27 15:22:05 -0400

    allow myself to use nvenc hevc in preparation for youtube rtmp hevc support

--- src/edit-widget.cpp
+++ src/edit-widget.cpp
@@ -410,12 +410,10 @@ public:
         };
         venc_->addItem(obs_module_text("SameAsOBS"), "");
         for(auto x : EnumEncodersByCodec("h264"))
-            venc_->addItem(ui_text(x).c_str(), x.c_str());
-        // for(auto x : EnumEncodersByCodec("av1"))
-        //     venc_->addItem(ui_text(x).c_str(), x.c_str());
-        // for(auto x : EnumEncodersByCodec("hevc"))
-        //     venc_->addItem(ui_text(x).c_str(), x.c_str());
-
+            venc_->addItem(obs_encoder_get_display_name(x.c_str()), x.c_str());
+        for(auto x : EnumEncodersByCodec("hevc"))
+            venc_->addItem(obs_encoder_get_display_name(x.c_str()), x.c_str());
+        
         aenc_->addItem(obs_module_text("SameAsOBS"), "");
         for(auto x : EnumEncodersByCodec("aac"))
             aenc_->addItem(ui_text(x).c_str(), x.c_str());
